264279645bf0f986b7e1ce61d1d3168ed15f1fa6
Kurimo1122 <kugimoto1122@gmail.com>
2016-10-21 20:20:51 +0900

    My Portfolio

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import logging
+from flask import Flask, session, redirect, render_template, request
+
+
+logging.warn('app start!')
+
+# Start Flask
+app = Flask(__name__)
+
+# Set key to use session of flask
+# app.secret_key = os.environ['SECRET_KEY']
+
+# Set root page
+@app.route('/')
+def index():
+
+	return render_template('index.html')
+
+#if __name__ == '__main__':
+#	app.run()
